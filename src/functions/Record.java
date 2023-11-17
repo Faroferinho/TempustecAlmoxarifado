01c9939d44749ea02d41c3da17460195364c90f8
Faroferinho <conradoperini@gmail.com>
2023-11-17 11:53:59 -0300

    Inicialização do Sistema de Histórico

--- src/functions/Record.java
+++ src/functions/Record.java
@@ -1,5 +1,11 @@
 package functions;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 public class Record {
+	
+	public File history = new File(ClassLoader.getSystemResource("changeLog.txt").getFile());
+	public FileInputStream log;
 
 }
