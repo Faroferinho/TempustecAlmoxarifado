3bb180d144af247c880ae2f9434d5718fd00974b
faroferinho <conradoperini@gmail.com>
2024-04-25 11:05:49 -0300

    Sistema de renderização e lógica de seleção concluidos

--- src/pages/AddPart.java
+++ src/pages/AddPart.java
@@ -8,6 +8,7 @@ import java.awt.Rectangle;
 import functions.DBConector;
 import functions.Functions;
 import functions.Insertions;
+import main.Almoxarifado;
 import main.UserInterface;
 
 public class AddPart extends Insertions {
@@ -16,6 +17,8 @@ public class AddPart extends Insertions {
 		// TODO Auto-generated constructor stub
 		quantity = 5;
 		border = 15;
+		
+		Title = "Cadastro de Peça:";
 		
 		labels.add("O.S. da Peça:");
 		values.add(DBConector.readDB("ISO", "Montagem ORDER BY LENGTH(ISO) DESC LIMIT 1").replaceAll(" § ", ""));
@@ -29,7 +32,7 @@ public class AddPart extends Insertions {
 		values.add(DBConector.readDB("Supplier", "Pecas ORDER BY LENGTH(Supplier) DESC LIMIT 1").replaceAll(" § ", ""));
 		
 		for(int i = 0; i < quantity; i++) {
-			textBoxes.add(new Rectangle(50, 180 + 100 * i, values.get(i).length() * 8 + 24, 40));
+			textBoxes.add(new Rectangle(80, 200 + 90 * i, values.get(i).length() * 8 + 24, 40));
 			values.set(i, "");
 		}
 	}
@@ -54,16 +57,9 @@ public class AddPart extends Insertions {
 	protected void writeOnTextBoxes(Graphics g) {
 		// TODO Auto-generated method stub
 		
-		g.setFont(new Font("segoi ui", 0, 15));
+		
 		g.setColor(Color.magenta);
 		
-		if(selected != -1) {
-			for(int i = 0; i < quantity; i++) {
-				if(selected == i) {
-					g.drawString(values.get(i), textBoxes.get(i).x + 12, textBoxes.get(i).y + g.getFontMetrics().getHeight() + 8);
-				}
-			}
-		}
 	}
 
 	@Override
@@ -75,18 +71,31 @@ public class AddPart extends Insertions {
 	@Override
 	public void tick() {
 		// TODO Auto-generated method stub
+		if(isWriting && selected == -1) {
+			selected = 0;
+		}
+		
 		writeQuery();
 	}
 
 	@Override
 	public void render(Graphics g) {
 		// TODO Auto-generated method stub
-		g.setFont(new Font("segoi ui", 1, 18));
+		
 		g.setColor(Color.white);
+		g.setFont(new Font("segoi ui", 1, 40));
+		g.drawString(Title, Almoxarifado.WIDTH/2 - g.getFontMetrics().stringWidth(Title)/2, 160);
+		
 		
 		for(int i = 0; i < quantity; i++) {
+			g.setFont(new Font("segoi ui", 1, 18));
 			g.drawString(labels.get(i), textBoxes.get(i).x + 12, textBoxes.get(i).y - 5);
+			
 			UserInterface.createTextBox(g, textBoxes.get(i), border);
+			
+			g.setFont(new Font("segoi ui", 0, 15));
+			g.drawString(values.get(i), textBoxes.get(i).x + 12, textBoxes.get(i).y + g.getFontMetrics().getHeight() + 8);
+			
 			g.setColor(Color.white);
 		}
 		
